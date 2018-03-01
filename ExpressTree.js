29d2a5c1a78d055fe792ba7d87cce7a4b56ee10b
bryanhclark <bryanhclark@gmail.com>
2018-03-01 18:45:26 -0500

    added express tree solution

---
+++ ExpressTree.js
@@ -0,0 +1,55 @@
+//Expression tree
+
+
+const evaluateExpTree = (root) => {
+  if (!root) 0;
+  if (!root.left) return root.data
+
+  let leftSum = evaluateExpTree(root.left)
+  let rightSum = evaluateExpTree(root.right)
+
+  if (root.data === '+') return leftSum + rightSum
+  else if (root.data === '-') return leftSum - rightSum
+  else if (root.data === '*') return leftSum * rightSum
+  else return leftSum / rightSum
+}
+
+const myExpTree = {
+  data: '+',
+  left: {
+    data: '*',
+    left: {
+      data: 5,
+      left: null,
+      right: null
+    },
+    right: {
+      data: 4,
+      left: null,
+      right: null
+    }
+  },
+  right: {
+    data: '-',
+    left: {
+      data: 100,
+      left: null,
+      right: null
+    },
+    right: {
+      data: '/',
+      left: {
+        data: 20,
+        left: null,
+        right: null
+      },
+      right: {
+        data: 2,
+        left: null,
+        right: null
+      }
+    }
+  }
+}
+
+console.log(evaluateExpTree(myExpTree))
